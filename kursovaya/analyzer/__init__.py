044fabcd8307fb61bb0a1dbef3cff9ec1abebf2f
Daniil <dan.bardyuk@yandex.ru>
2021-12-13 15:55:26 +0300

    init and add db

---
+++ kursovaya/analyzer/__init__.py
@@ -0,0 +1,15 @@
+__author__ = 'Daniil Bardyuk'
+__maintainer__ = __author__
+
+__email__ = 'dan.bardyuk@yandex.ru'
+__license__ = 'MIT'
+__version__ = '0.0.1'
+
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__',
+)
